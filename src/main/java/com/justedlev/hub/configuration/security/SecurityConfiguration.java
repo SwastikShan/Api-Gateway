c761b146ae7786c736f614b1e146f3a213865ea7
SwastikShan <kumarshantanu4403@gmail.com>
2024-02-06 15:40:24 +0530

    first commit

---
+++ src/main/java/com/justedlev/hub/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.justedlev.hub.configuration.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(@NonNull ServerHttpSecurity httpSecurity) {
+        return httpSecurity
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .oauth2ResourceServer(serverSpec -> serverSpec.jwt(Customizer.withDefaults()))
+                .logout(logoutHandler -> logoutHandler
+                        .logoutHandler(keycloakLogoutHandler)
+                        .logoutUrl("/logout")
+                        .logoutSuccessHandler((exchange, authentication) -> {
+                            log.debug("{}", authentication);
+                            return Mono.empty();
+                        })
+                )
+                .authorizeExchange(exchangeSpec -> exchangeSpec
+                        .pathMatchers(
+                                "/webjars/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/actuator/**",
+                                "/error",
+                                "/sso/logout",
+                                "/logout"
+                        ).permitAll()
+                        .anyExchange().authenticated()
+                )
+                .build();
+    }
+}
